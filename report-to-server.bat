b347121ba5d7ad84dabdb0fbc2de7608357fd24f
Shivam <shivam.kumar@go-jek.com>
2020-08-24 23:44:41 +0530

    added file support

--- report-to-server.bat
+++ report-to-server.bat
@@ -2,4 +2,4 @@ echo Pushing Allure-results to server and updating results
 tar.exe -czf allure-results-zip.tar.gz allure-results
 curl -L -X POST 'http://localhost:8010/v1/allure/addresults/user=admin/password=admin123' -F 'file=@allure-results-zip.tar.gz'
 echo .
-echo Server url: http://18.219.242.102:37982/index.html#
+echo Server url: http://localhost:8080/index.html#
